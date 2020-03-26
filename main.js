bd9b93467927113a2bbb052dca0d42f1f6ce5216
amie-nzie <costlystone@gmail.com>
2020-03-26 21:20:41 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let user = {
+    firstName: 'Amarachi'
+};
+user.lastName = 'Eke';
+console.log (user)
+let newName = user.firstName +  " " + user.lastName
+console.log(newName)
+
+var courses = ["HTML", "CSS", "Javascript", "Flutter", "Kotlin"]
+console.log(courses)
+
+var a=0;
+for(a=0;a<=200;a++){
+    if(a%2!=0){
+        console.log(a);
+    }
+}
